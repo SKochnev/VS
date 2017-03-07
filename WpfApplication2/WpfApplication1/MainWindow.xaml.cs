35a1d1fc5a95d5e01a696f05722f896c2c1a7cfa
SKochnev <krickdontone@gmail.com>
2017-03-08 01:27:16 +1000

    ДЗ первой

---
+++ WpfApplication2/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttonStudInDel.Content = ">";
+            buttonStudFromDel.Content = "<";
+
+            List<Student> items = new List<Student>();
+            items.Add(new Student() { FIO = "Жопкин Анатолий Владимирович", Group = "ОрГу-11", Phone = "89012387631" });
+            Stud.ItemsSource = items;
+        }
+
+        public class Student
+        {
+            public string FIO { get; set; }
+
+            public string Group { get; set; }
+
+            public string Phone { get; set; }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
